be16eb0e7eaaeccaf1a0f9705c1733cd3b5eae70
salman <clark.green25@gmail.com>
2018-03-15 12:29:51 +0530

    added circle ci config for test

---
+++ .circleci/install-power-shell.sh
@@ -0,0 +1,16 @@
+set -x
+set -e
+# Download the powershell '.tar.gz' archive
+curl -L -o /tmp/powershell.tar.gz https://github.com/PowerShell/PowerShell/releases/download/v6.0.0/powershell-6.0.0-linux-x64.tar.gz
+
+# Create the target folder where powershell will be placed
+sudo mkdir -p /opt/microsoft/powershell/6.0.0
+
+# Expand powershell to the target folder
+sudo tar zxf /tmp/powershell.tar.gz -C /opt/microsoft/powershell/6.0.0
+
+# Set execute permissions
+sudo chmod +x /usr/local/microsoft/powershell/6.0.0/pwsh
+
+# Create the symbolic link that points to pwsh
+sudo ln -s /opt/microsoft/powershell/6.0.0/pwsh /usr/bin/pwsh
